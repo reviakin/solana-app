158bfa075934619bf87696e633584eeb414a5f9c
TheHesoyam <reviakindi@gmail.com>
2022-12-09 13:49:25 +0200

    store data in account

--- programs/solana-app/src/lib.rs
+++ programs/solana-app/src/lib.rs
@@ -5,11 +5,41 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solana_app {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // add a GIF
+    pub fn add_gif(ctx: Context<AddGif>) -> Result<()> {
+        // get a reference to the account and incremenet total_gifs
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
         Ok(())
     }
 }
 
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// attach vars into Initialize contxt
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// tell solana what we want to store in account
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/solana-app.js
+++ tests/solana-app.js
@@ -1,13 +1,45 @@
 const anchor = require("@project-serum/anchor");
 
+const { SystemProgram } = anchor.web3;
+
 describe("solana-app", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   it("Is initialized!", async () => {
     // Add your test here.
     const program = anchor.workspace.SolanaApp;
-    const tx = await program.methods.initialize().rpc();
+
+    // generate an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    //const tx = await program.methods.initialize().rpc();
+    let tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+    //const tx = await program.rpc.initialize
     console.log("Your transaction signature", tx);
+    // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.log("👀 GIF Count", account.totalGifs.toString());
+
+    // Call add_gif!
+    await program.rpc.addGif({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    // Get the account again to see what changed.
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("👀 GIF Count", account.totalGifs.toString());
   });
 });
